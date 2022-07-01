81cc80375188f6a61d98096f5e287948b57d8e8f
Yuta <yuuta.dev@gmail.com>
2022-07-01 10:39:57 +0300

    propper usage of constant values

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const JWT_SECRET = 'secret'
+export const SALT_ROUNDS = 8
+export const MIN_USER_LENGTH = 4
+export const MAX_USER_LENGTH = 20
+export const MIN_PASSWORD_LENGTH = 8
--- src/index.ts
+++ src/index.ts
@@ -1,173 +1,153 @@
-import express, { application } from "express";
-import { User } from "./models/user";
-import bodyParser from "body-parser";
-import bcrypt from "bcrypt";
-import { dataSource as db } from "./dataSource";
+import express, { application } from 'express'
+import { User } from './models/user'
+import bodyParser from 'body-parser'
+import bcrypt from 'bcrypt'
+import { dataSource as db } from './dataSource'
+import { MAX_USER_LENGTH, MIN_PASSWORD_LENGTH, MIN_USER_LENGTH, SALT_ROUNDS } from './constants'
 
 const bootstrap = async () => {
-  await db.initialize();
+	await db.initialize()
 
-  const app = express();
+	const app = express()
 
-  app.use(bodyParser.json());
+	app.use(bodyParser.json())
 
-  app.get("/users", async (_, res) => {
-    res.status(200).send(await User.find());
-  });
+	app.get('/users', async (_, res) => {
+		res.status(200).send(await User.find())
+	})
 
-  app.post("/users", async (req, res) => {
-    const { username, password, email, firstName, lastName } = req.body;
+	app.post('/users', async (req, res) => {
+		const { username, password, email, firstName, lastName } = req.body
 
-    if (!username || !password || !email) {
-      res.status(400).send({ message: "Missing required fields" });
-      return;
-    }
+		if (!username || !password || !email) {
+			res.status(400).send({ message: 'Missing required fields' })
+			return
+		}
 
-    const userName = await User.findOne({ where: { username } });
-    const userEmail = await User.findOne({ where: { email } });
-    if (userName || userEmail) {
-      res.status(400).send({ message: "User already exists" });
-      return;
-    }
+		const userName = await User.findOne({ where: { username } })
+		const userEmail = await User.findOne({ where: { email } })
+		if (userName || userEmail) {
+			res.status(400).send({ message: 'User already exists' })
+			return
+		}
 
-    const newUser = new User();
-    newUser.firstName = firstName;
-    newUser.lastName = lastName;
+		const newUser = new User()
+		newUser.firstName = firstName
+		newUser.lastName = lastName
 
-    if (username.length <= 4 && username.length > 20) {
-      res.status(400).send({
-        message:
-          "Username must be at least 4 characters and less than 20 characters",
-      });
+		if (username.length <= MIN_USER_LENGTH && username.length > MAX_USER_LENGTH) {
+			res.status(400).send({
+				message: 'Username must be at least 4 characters and less than 20 characters',
+			})
 
-      return;
-    }
+			return
+		}
 
-    if (username.includes(" ")) {
-      res.status(400).send({ message: "Username cannot contain spaces" });
-      return;
-    }
+		if (username.includes(' ')) {
+			res.status(400).send({ message: 'Username cannot contain spaces' })
+			return
+		}
 
-    newUser.username = username;
+		newUser.username = username
 
-    if (password.length <= 8) {
-      res
-        .status(400)
-        .send({ message: "Password must be at least 8 characters" });
-      return;
-    }
+		if (password.length <= MIN_PASSWORD_LENGTH) {
+			res.status(400).send({ message: 'Password must be at least 8 characters' })
+			return
+		}
 
-    const hashedPassword = await bcrypt.hash(password, 8);
-    newUser.password = hashedPassword;
+		const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+		newUser.password = hashedPassword
 
-    newUser.email = email;
+		newUser.email = email
 
-    await newUser.save();
-    res.status(201).send(newUser);
-  });
+		await newUser.save()
+		res.status(201).send(newUser)
+	})
 
-  app.patch("/users", async (req, res) => {
-    const {
-      username,
-      newUsername,
-      password,
-      newPassword,
-      email,
-      firstName,
-      lastName,
-    } = req.body;
+	app.patch('/users', async (req, res) => {
+		const { username, newUsername, password, newPassword, email, firstName, lastName } = req.body
 
-    if (!username || !password) {
-      res
-        .status(400)
-        .send({ message: "You must provide a valid username and a password" });
-      return;
-    }
+		if (!username || !password) {
+			res.status(400).send({ message: 'You must provide a valid username and a password' })
+			return
+		}
 
-    const user = await User.findOne({ where: { username } });
-    if (!user) {
-      res.status(404).send({ message: "User not found" });
-      return;
-    }
+		const user = await User.findOne({ where: { username } })
+		if (!user) {
+			res.status(404).send({ message: 'User not found' })
+			return
+		}
 
-    const verifyPass = await bcrypt.compare(password, user.password);
-    if (!verifyPass) {
-      res.status(403).send({ message: "Incorrect password" });
-      return;
-    }
+		const verifyPass = await bcrypt.compare(password, user.password)
+		if (!verifyPass) {
+			res.status(403).send({ message: 'Incorrect password' })
+			return
+		}
 
-    if (firstName) {
-      user.firstName = firstName;
-    }
+		if (firstName) {
+			user.firstName = firstName
+		}
 
-    if (lastName) {
-      user.lastName = lastName;
-    }
+		if (lastName) {
+			user.lastName = lastName
+		}
 
-    if (email) {
-      user.email = email;
-    }
+		if (email) {
+			user.email = email
+		}
 
-    if (newUsername) {
-      if (newUsername.length <= 4) {
-        res
-          .status(400)
-          .send({ message: "Username must be at least 4 characters" });
-        return;
-      }
+		if (newUsername) {
+			if (newUsername.length <= MIN_USER_LENGTH && newUsername.length > MAX_USER_LENGTH) {
+				res.status(400).send({
+					message: 'Username must be at least 4 characters and less than 20 characters',
+				})
 
-      if (newUsername.length > 20) {
-        res
-          .status(400)
-          .send({ message: "Username must be less than 20 characters" });
-        return;
-      }
+				return
+			}
 
-      if (newUsername.includes(" ")) {
-        res.status(400).send({ message: "Username cannot contain spaces" });
-        return;
-      }
+			if (newUsername.includes(' ')) {
+				res.status(400).send({ message: 'Username cannot contain spaces' })
+				return
+			}
 
-      user.username = newUsername;
-    }
+			user.username = newUsername
+		}
 
-    if (newPassword) {
-      if (newPassword.length <= 8) {
-        res
-          .status(400)
-          .send({ message: "Password must be at least 8 characters" });
-        return;
-      }
+		if (newPassword) {
+			if (newPassword.length <= MIN_PASSWORD_LENGTH) {
+				res.status(400).send({ message: 'Password must be at least 8 characters' })
+				return
+			}
 
-      const hashedPassword = await bcrypt.hash(newPassword, 8);
-      user.password = hashedPassword;
-    }
+			const hashedPassword = await bcrypt.hash(newPassword, SALT_ROUNDS)
+			user.password = hashedPassword
+		}
 
-    await user.save();
-    res.status(200).send(user);
-  });
+		await user.save()
+		res.status(200).send(user)
+	})
 
-  app.delete("/users", async (req, res) => {
-    const { username } = req.body;
-    if (!username) {
-      res.status(400).send({ message: "Missing required fields" });
-      return;
-    }
+	app.delete('/users', async (req, res) => {
+		const { username } = req.body
+		if (!username) {
+			res.status(400).send({ message: 'Missing required fields' })
+			return
+		}
 
-    const user = await User.findOne({ where: { username } });
-    if (!user) {
-      res.status(404).send({ message: "User not found" });
-      return;
-    }
+		const user = await User.findOne({ where: { username } })
+		if (!user) {
+			res.status(404).send({ message: 'User not found' })
+			return
+		}
 
-    await user.remove();
-    res.status(200).send({ message: true });
-  });
+		await user.remove()
+		res.status(200).send({ message: true })
+	})
 
-  const port = 4000;
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
-};
+	const port = 4000
+	app.listen(port, () => {
+		console.log(`Server listening on port ${port}`)
+	})
+}
 
-bootstrap().catch((err) => console.error(err));
+bootstrap().catch(err => console.error(err))
