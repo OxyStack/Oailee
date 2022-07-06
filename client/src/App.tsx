f48fd361bf1669c0909d67dd1036b444c0f1e029
mizuioshi <baland596@gmail.com>
2022-07-06 12:53:26 +0300

    Propper client side structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './views/Home'
-import About from './views/About'
+import { Home, About, Dashboard, Profile, SignUp, SignIn } from './views'
+import { Footer, Header, Navbar } from './components'
 
 function App() {
 	return (
@@ -8,6 +8,13 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/Profile" element={<Profile />} />
+				<Route path="/SignUp" element={<SignUp />} />
+				<Route path="/SignIn" element={<SignIn />} />
+				<Route path="/Header" element={<Header />} />
+				<Route path="/Navbar" element={<Navbar />} />
+				<Route path="/Footer" element={<Footer />} />
 			</Routes>
 		</BrowserRouter>
 	)
